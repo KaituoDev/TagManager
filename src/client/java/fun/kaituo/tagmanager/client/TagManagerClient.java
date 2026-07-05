9184512967ea20d5d9c416de66a8ed962e435d2d
LeChocolatChaud <mazihan1024@hotmail.com>
2026-07-06 01:14:28 +0800

    feat: initial commit

---
+++ src/client/java/fun/kaituo/tagmanager/client/TagManagerClient.java
@@ -0,0 +1,10 @@
+package fun.kaituo.tagmanager.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TagManagerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
